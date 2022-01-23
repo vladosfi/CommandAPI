40a8a194dce3922e64419340bc08b40913667f84
Vladimir Ivanov <vgivanov@gmail.com>
2022-01-23 19:38:26 +0200

    Added CommandContext to solution

---
+++ src/CommandAPI/Data/SqlCommandAPIRepo.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using CommandAPI.Models;
+using System.Linq;
+
+namespace CommandAPI.Data
+{
+    public class SqlCommandAPIRepo : ICommandAPIRepo
+    {
+        private readonly CommandContext context;
+        public SqlCommandAPIRepo(CommandContext context)
+        {
+            this.context = context;
+        }
+
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            return this.context.CommandItems.ToList();
+        }
+        public Command GetCommandById(int id)
+        {
+            return this.context.CommandItems.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
