1b9d84ca2a7a31ea1907f61d9104e2caf2d2696a
vladosfi <vladosfi@gmail.com>
2022-01-28 13:45:57 +0200

    Fix failing test

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -299,7 +299,7 @@ namespace CommandAPI.Tests
             var result = controller.DeleteCommand(0);
 
             //Assert
-            Assert.IsType<OkResult>(result);
+            Assert.IsType<NotFoundResult>(result);
         }
 
     }
