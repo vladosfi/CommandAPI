5708f14a83b367957c4d1c25cdd70cb185a2d9ff
vladosfi <vladosfi@gmail.com>
2022-01-31 10:32:13 +0200

    Trigger the Build Pipeline

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -24,6 +24,7 @@ namespace CommandAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             var builder = new NpgsqlConnectionStringBuilder();
             builder.ConnectionString =
             this.Configuration.GetConnectionString("PostgreSqlConnection");
